5d57dd89cd3db0e9db8522ba9fda3563be3bc869
piya03 <piyachaudhary50@gmail.com>
2020-02-07 22:08:29 +0530

    button

--- src/myproject/index.js
+++ src/myproject/index.js
@@ -1,18 +1,21 @@
-import React from 'react';
-import Header from './header.js'
+import React from "react";
+import Header from "./header.js";
 
-class GoogleCalenderMain extends React.Component{
-    constructor(props) {
-        super(props);
-      }
-    render() {
-        return (
-            <div>
-           <Header />
+class GoogleCalenderMain extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div>
+        <Header />
 
-            </div>
-
-        );
-    }
+        <div className='createbtn'>
+        <svg width="36" height="36" viewBox="0 0 36 36"><path fill="#34A853" d="M16 16v14h4V20z"></path><path fill="#4285F4" d="M30 16H20l-4 4h14z"></path><path fill="#FBBC05" d="M6 16v4h10l4-4z"></path><path fill="#EA4335" d="M20 16V6h-4v14z"></path><path fill="none" d="M0 0h36v36H0z"></path></svg>
+        <span>Create</span>
+        </div>
+      </div>
+    );
+  }
 }
 export default GoogleCalenderMain;
